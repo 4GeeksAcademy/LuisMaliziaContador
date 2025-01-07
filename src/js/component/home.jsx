1071e9eee3bf9e036c20b83e5b1624a0bba1d349
Luis Malizia <luisfmaliziam@gmail.com>
2025-01-07 21:34:49 +0100

    contador terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center my-5">
+			<div className="row justify-content-center">
+				<div className="card col-2 d-flex mx-1  justify-content-center">
+					<span className="card-title m-3">
+						<i class="bi bi-stopwatch"></i>
+					</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n6}</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n5}</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n4}</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n3}</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n2}</span>
+				</div>
+				<div className="card col-1 d-flex mx-1 justify-content-center">
+					<span className="card-title m-3">{props.n1}</span>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default home;
